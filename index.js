97ca31bbb4bea04ab371f979ef68dc06d42f3a4b
Andrew Schwartz <andrew@aschwartz.me>
2019-04-16 17:55:36 -0600

    implement CORS to allow the current server to make requests

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const Mercury = require('@postlight/mercury-parser');
 const Express = require('express');
 const cheerio = require('cheerio')
-
+const cors = require('cors');
 
 const port = 5555;
 const app = Express();
 
-app.get('/card/', (req, res) => {
+var corsOptions = {
+  origin: 'http://51.15.99.218',
+  optionsSuccessStatus: 200
+}
+
+app.get('/card/', cors(corsOptions), function (req, res) {
   url = req.query.url;
   Mercury.parse(url, { contentType: 'text'}).then(function (mercuryResult) {
     res.status(200).send({
